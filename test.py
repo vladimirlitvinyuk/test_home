eef1f0fc6743ad228796ba5fa22e6d83d0f7d74c
Your Name <you@example.com>
2015-05-17 14:51:57 -0400

    from server

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 __author__ = 'samsung'
 import sys
+i = 0
 for line in sys.stdin:
         for token in line.strip().split():
             print(token + "\t 1")
+            i = i+1
+            if i>50:
+		 break
